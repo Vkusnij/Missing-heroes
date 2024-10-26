b2e8a41ed175d3476e8df86f9c481d40c9395dfb
appuser <appuser@journey2-6df5db96fc-6whqm>
2024-10-26 08:32:08 +0000

    starter project

---
+++ corrections.py
@@ -0,0 +1,7 @@
+from data import movies
+
+# You can use the `movies` array here
+# Please write every correction and modification of the data to this file by updating the `movies` array
+
+# WRITE YOUR CODE HERE
+
